551a39b1ee142e2962b13ba53d5ed92a42e286e5
ariefahmad <37898149+ariefahmad@users.noreply.github.com>
2018-08-29 15:38:36 +0100

    Update set1challenge4.py

--- set1challenge4.py
+++ set1challenge4.py
@@ -7,13 +7,12 @@ import pandas as pd
 
 if __name__ == '__main__':
     reader = pd.read_csv("4.txt", delimiter=" ", header=None)
-
     for lines in range(0, len(reader[0])):
         print("Line: ", lines)
-        print(set1challenge3.crack_singlebyteXOR(reader[0][lines]))
+        print(set1challenge3.crack_singlebyteXOR(bytes.fromhex(reader[0][lines])))
 
 # Output:
 # Line:  170
 # (53, 2.3613299999999993, b'Now that the party is jumping\n')
 
-    print(set1challenge3.crack_singlebyteXOR(reader[0][170])[2].decode("ascii"))  # final formatting
+    print(set1challenge3.crack_singlebyteXOR(bytes.fromhex(reader[0][170]))[2].decode("ascii"))  # final formatting
