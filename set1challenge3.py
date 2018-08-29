9e9813918da1b2e1305f01e95a55d53424effb5f
ariefahmad <37898149+ariefahmad@users.noreply.github.com>
2018-08-29 08:23:49 +0100

    Update set1challenge3.py

--- set1challenge3.py
+++ set1challenge3.py
@@ -72,7 +72,7 @@ def crack_singlebyteXOR(cipher_str):
 if __name__ == '__main__':
     encodedA = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
-    print(crack_singlebyteXOR(encodedA))
+    print(crack_singlebyteXOR(encodedA)[2].decode("ascii"))
 
 # Output:
 # (88, 2.5316499999999995, b"Cooking MC's like a pound of bacon")
